f88380dc26089c8eb1a101d739a6f3543a28218c
CHAYATORN KUKATTIKOON <chayatornkukattikoon@CHAYATORNs-MacBook-Air.local>
2024-08-27 22:50:58 +0700

    update image, sizeing and position of information

--- src/app/product/[productId]/components/LocationInformationNew.tsx
+++ src/app/product/[productId]/components/LocationInformationNew.tsx
@@ -1,5 +1,5 @@
 'use client';
-import BGFarm from '@/app/assets/bg-farms.webp';
+import BGFarm from '@/app/assets/bg-farms.png';
 import { FiMapPin } from 'react-icons/fi';
 import { LuMap } from 'react-icons/lu';
 
@@ -38,25 +38,55 @@ export default function LocationInformationNew({
       ></div>
       <div className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-white w-full'>
         <div className='w-full flex flex-col items-center justify-center text-md leading-[24px]'>
-          <div className='flex gap-2 items-center mb-2'>
-            <p className='text-xl font-bold'>Location</p> <FiMapPin />
+          <div className='w-[300px] flex flex-col items-center justify-center '>
+            <div className='flex gap-2 items-center mb-2'>
+              <p className='text-xl font-bold'>Location</p> <FiMapPin />
+            </div>
+            <table className='table-auto w-full text-center'>
+              <tbody>
+                <tr>
+                  <th className='text-left'>Country</th>
+                  <td>{country}</td>
+                </tr>
+                <tr>
+                  <th className='text-left'>Province</th>
+                  <td>{province}</td>
+                </tr>
+                <tr>
+                  <th className='text-left'>District</th>
+                  <td>{district}</td>
+                </tr>
+                <tr>
+                  <th className='text-left'>Farm</th>
+                  <td>{farmName}</td>
+                </tr>
+                <tr>
+                  <th className='text-left'>Size</th>
+                  <td>
+                    {size} {sizeUnit}
+                  </td>
+                </tr>
+                <tr>
+                  <th className='text-left'>Established</th>
+                  <td>{establish}</td>
+                </tr>
+                <tr>
+                  <th className='text-left'>Type of Farm</th>
+                  <td>{type}</td>
+                </tr>
+                <tr>
+                  <th className='text-left'>Climate</th>
+                  <td>{climate}</td>
+                </tr>
+              </tbody>
+            </table>
+            <button
+              onClick={mapBtnClick}
+              className='btn mt-8 glass text-base-200 hover:text-base-content  whitespace-nowrap'
+            >
+              <LuMap /> See on Map!
+            </button>
           </div>
-          <p>Country: {country}</p>
-          <p>Province: {province}</p>
-          <p>District: {district}</p>
-          <p>Farm: {farmName}</p>
-          <p>
-            Size: {size} {sizeUnit}
-          </p>
-          <p>Established: {establish}</p>
-          <p>Type of Farm: {type}</p>
-          <p>Climate: {climate}</p>
-          <button
-            onClick={mapBtnClick}
-            className='btn mt-8 glass text-base-200 hover:text-base-content  whitespace-nowrap'
-          >
-            <LuMap /> See on Map!
-          </button>
         </div>
       </div>
     </div>
--- src/app/product/[productId]/components/SaleInformation.tsx
+++ src/app/product/[productId]/components/SaleInformation.tsx
@@ -1,4 +1,4 @@
-import BGFarm from '@/app/assets/desk.webp';
+import BGFarm from '@/app/assets/desk.png';
 import { MdOutlineLocalShipping } from 'react-icons/md';
 import { IoAirplaneOutline } from 'react-icons/io5';
 import { BsShop } from 'react-icons/bs';
@@ -11,32 +11,43 @@ export default function SaleInformation() {
         style={{ backgroundImage: `url(${BGFarm.src})`, opacity: 0.4 }}
       ></div>
       <div className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-white w-full'>
-        <div className='w-full flex flex-col justify-center text-md leading-[24px] px-8 text-left'>
-          <div className='flex gap-2 items-center mb-2'>
-            <p className='text-xl font-bold'>Sales and Distribution:</p>{' '}
-            <MdOutlineLocalShipping />
+        <div className='w-full flex flex-col justify-center items-center text-md leading-[24px] text-left'>
+          <div className='w-[300px]'>
+            <div className='flex gap-2 items-center mb-4'>
+              <p className='text-xl font-bold'>Sales and Distribution:</p>
+              <MdOutlineLocalShipping />
+            </div>
+            <div className='mb-4'>
+              <div className='flex gap-2 items-center mb-2'>
+                <BsShop />
+                <h6 className='font-semibold'>Local Markets</h6>
+              </div>
+              <ol className='pl-6 list-disc'>
+                <li>Mae Rim Organic Market</li>
+                <li>Chiang Mai Farmers Market</li>
+              </ol>
+            </div>
+            <div className='mb-4'>
+              <div className='flex gap-2 items-center mb-2'>
+                <IoAirplaneOutline />
+                <h6 className='font-semibold'>Export Markets</h6>
+              </div>
+              <ol className='pl-6 list-disc'>
+                <li>Dubai</li>
+                <li>China</li>
+                <li>Vietnam</li>
+              </ol>
+            </div>
+            <div>
+              <div className='flex gap-2 items-center mb-2'>
+                <RiShoppingBasketLine />
+                <h6 className='font-semibold'>Online Sales</h6>
+              </div>
+              <ol className='pl-6 list-disc'>
+                <li>Available on farm website and major online marketplaces</li>
+              </ol>
+            </div>
           </div>
-
-          <div className='flex gap-2 items-center'>
-            <BsShop />
-            <h6>Local Markets</h6>
-          </div>
-          <p className='mt-2'>- Mae Rim Organic Market</p>
-          <p className='mb-4'>- Chiang Mai Farmers Market</p>
-          <div className='flex gap-2 items-center'>
-            <IoAirplaneOutline />
-            <h6>Export Markets</h6>
-          </div>
-          <p className='mt-2'>- Japan</p>
-          <p>- USA</p>
-          <p className='mb-4'>- Germany</p>
-          <div className='flex gap-2 items-center'>
-            <RiShoppingBasketLine />
-            <h6>Online Sales</h6>
-          </div>
-          <p className='mt-2 mb-4'>
-            - Available on farm website and major online marketplaces
-          </p>
         </div>
       </div>
     </div>
